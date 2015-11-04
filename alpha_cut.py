a52256e848fb369f7aeac1cb9e2952e9d6ec0531
Jan Sabsch <sabsch@st.ovgu.de>
2015-11-04 14:39:04 +0100

    insert intervals and handle errors

--- alpha_cut.py
+++ alpha_cut.py
@@ -6,13 +6,35 @@ class Alpha_Cut:
         self.__level = level
         self.__intervals = []
 
-    def add_interval(self, min_b, max_b):
-        self.__intervals.append((min_b,max_b))
-
+    def add_intervals(self, inter_list):
+        
+        for i in inter_list:
+            self.__intervals.append(i)
+            
+        return False
+    
+    def print_level(self):
+        print "alpha cut at: ",self.__level
 
     def print_cut(self):
         print self.__level
         for i in self.__intervals:
             print i[0], ",", i[1]
             
-    def check_consistency(self):
+    def check_consistency(self, tuple_list):
+        
+        for t in tuple_list:
+            is_consistent = False
+            
+            for interval in self.__intervals:
+                if t[0] >= interval[0] and t[1] <= interval[1]:
+                    is_consistent = True
+                    break   # good
+            
+            if not is_consistent:
+                print "Intervals not consistent."
+                return False
+            
+        return True
+        
+        
--- start_alpha.py
+++ start_alpha.py
@@ -1,28 +1,90 @@
 #!/usr/bin/env python
 
 import alpha_cut
+from duplicity.commandline import check_consistency
 
 
+def insert_alpha_cuts():
+    try:
+        eingabe = input("Insert a list of alpha cuts: ")
+        
+        for e in eingabe:
+            if e > 0 and e <= 1:
+                cuts.append(alpha_cut.Alpha_Cut(e))
+                
+    except NameError:
+        print "wrong input."
+    except TypeError:
+        print "please insert more than one alpha cut."
+        # TODO more than one legit alpha cut!
+        # TODO sort alpha cuts!
+        
+    return cuts
+        
 
-def check_consistency():
-    pass
 
-try:
-    eingabe = input("Insert a list of alpha cuts: ")
-    
-    cuts = []
-    for e in eingabe:
-        if e >= 0 and e <= 1:
-            cuts.append(alpha_cut.Alpha_Cut(e))
-    
-    cuts[0].add_interval(1,2)
-    cuts[0].add_interval(3,4)
+def insert_intervals():
+    lower_cut = 0
     
     for c in cuts:
-        c.print_cut()
+        c.print_level()
+        #TODO type check
+        input2 = input("Please insert intervals for this alpha cut as a list of tupels: ")
+        if not check_input(input2, lower_cut):
+            return False
         
-except NameError:
-    print "wrong input."
-except TypeError:
-    print "please insert more than one alpha cuts."
+        lower_cut = c
+        c.add_intervals(input2)
 
+def check_input(intervals, lower_cut):
+    if type(intervals) is not tuple:
+        print "wrong input type! (1)"
+        return False
+        
+    #sort
+    tuple_list = []
+    try:
+        sort_intervals(intervals, tuple_list)
+    except TypeError as e:
+        print "wrong input type! (2)"
+        return False
+    
+    #overlapping
+    try:
+        overlaps(tuple_list)
+    except ValueError:
+        print "intervals overlap!"
+        return False
+    
+    return check_consistency(tuple_list, lower_cut)
+    
+def check_consistency(tuple_list, lower_cut):
+    if lower_cut == 0:
+        return True
+    
+    return lower_cut.check_consistency(tuple_list)
+        
+
+def sort_intervals(intervals, tuple_list):
+    for i in intervals:
+        if type(i) is not tuple:
+            raise TypeError('wrong input type')
+        tuple_list.append(i)
+    
+    tuple_list.sort()
+    return True
+    
+def overlaps(tuple_list):    
+    last = tuple_list[0][0] - 1
+    for t in tuple_list:
+        if t[0] <= last:
+            raise ValueError('Intervals overlap. Abort calculations.')
+        last = t[1]
+
+
+    
+cuts = []
+while not cuts:
+    insert_alpha_cuts()
+    
+insert_intervals()
