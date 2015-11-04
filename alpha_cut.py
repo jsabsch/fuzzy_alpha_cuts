b7f526c6347371cb71cfb1383dddb6b88f1d3464
Jan Sabsch <sabsch@st.ovgu.de>
2015-11-04 13:15:31 +0100

    "in between"-commit

--- alpha_cut.py
+++ alpha_cut.py
@@ -4,6 +4,15 @@ class Alpha_Cut:
     
     def __init__(self, level):
         self.__level = level
+        self.__intervals = []
 
-    def print_level(self):
+    def add_interval(self, min_b, max_b):
+        self.__intervals.append((min_b,max_b))
+
+
+    def print_cut(self):
         print self.__level
+        for i in self.__intervals:
+            print i[0], ",", i[1]
+            
+    def check_consistency(self):
--- start_alpha.py
+++ start_alpha.py
@@ -2,16 +2,27 @@
 
 import alpha_cut
 
+
+
+def check_consistency():
+    pass
+
 try:
     eingabe = input("Insert a list of alpha cuts: ")
     
     cuts = []
     for e in eingabe:
-        cuts.append(alpha_cut.Alpha_Cut(e))
+        if e >= 0 and e <= 1:
+            cuts.append(alpha_cut.Alpha_Cut(e))
+    
+    cuts[0].add_interval(1,2)
+    cuts[0].add_interval(3,4)
     
     for c in cuts:
-        c.print_level()
+        c.print_cut()
         
 except NameError:
     print "wrong input."
+except TypeError:
+    print "please insert more than one alpha cuts."
 
