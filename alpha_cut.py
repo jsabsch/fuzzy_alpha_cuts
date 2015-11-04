33e35771831d196e9ce23b7a003879bebab53446
alena-kathrin.schnurr <alena-kathrin.schnurr@st.ovgu.de>
2015-11-04 13:50:35 +0100

    membership function

--- alpha_cut.py
+++ alpha_cut.py
@@ -16,3 +16,18 @@ class Alpha_Cut:
             print i[0], ",", i[1]
             
     def check_consistency(self):
+        pass
+        
+    def membership(self,x):
+        for i in self.__intervals:
+            # if x is smaller than lower boundary
+            if i[0] > x:
+                return -1
+            # if x is smaller than upper boundary
+            if  i[1] >= x:
+                return self.__level
+        # if x is bigger than last upper boundary
+        return -1
+          
+                
+            
--- start_alpha.py
+++ start_alpha.py
@@ -7,6 +7,13 @@ import alpha_cut
 def check_consistency():
     pass
 
+def get_membership(cuts,x):
+    for cut in cuts:
+        m = cut.membership(x)
+        if m > -1:
+            return m
+    return 0
+
 try:
     eingabe = input("Insert a list of alpha cuts: ")
     
@@ -18,9 +25,16 @@ try:
     cuts[0].add_interval(1,2)
     cuts[0].add_interval(3,4)
     
+    cuts[1].add_interval(2,3)
+    cuts[2].add_interval(4,5)
+    
+    cuts[1].add_interval(5,6)
+    
     for c in cuts:
         c.print_cut()
         
+    print get_membership(cuts, 3.5)
+    
 except NameError:
     print "wrong input."
 except TypeError:
