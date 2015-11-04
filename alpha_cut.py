f1c1e213b4dbee84e0c992d84bb9c415e343b195
Jan Sabsch <sabsch@st.ovgu.de>
2015-11-04 12:21:07 +0100

    input 1

--- alpha_cut.py
+++ alpha_cut.py
@@ -4,3 +4,6 @@ class Alpha_Cut:
     
     def __init__(self, level):
         self.__level = level
+
+    def print_level(self):
+        print self.__level
--- start_alpha.py
+++ start_alpha.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python
+
+import alpha_cut
+
+try:
+    eingabe = input("Insert a list of alpha cuts: ")
+    
+    cuts = []
+    for e in eingabe:
+        cuts.append(alpha_cut.Alpha_Cut(e))
+    
+    for c in cuts:
+        c.print_level()
+        
+except NameError:
+    print "wrong input."
+
